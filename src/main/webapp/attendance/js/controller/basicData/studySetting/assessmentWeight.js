c1c66ccb86e17956f522b616309a091ac71872a4
mengyitf <mengyitf@163.com>
2018-06-09 10:34:31 +0800

    8

--- src/main/webapp/attendance/js/controller/basicData/studySetting/assessmentWeight.js
+++ src/main/webapp/attendance/js/controller/basicData/studySetting/assessmentWeight.js
@@ -67,4 +67,65 @@ function($scope, $rootScope, $state, $stateParams, $uibModal, $http, httpService
         });
     };
 
+    if ($rootScope.sign == 0) return;
+
+    $scope.weightOptions = [];
+
+    $scope.weightOptionsEdit = {delete: true};
+
+    $scope.weightEdit = false;
+
+    $scope.setWeightOptionsEdit = function (item) {
+        $scope.weightOptionsEdit = {delete: false};
+        $.extend(true, $scope.weightOptionsEdit, item);
+        console.log($scope.weightOptionsEdit);
+    };
+
+    $scope.pushItem = function () {
+        $scope.weightOptions.push($scope.weightOptionsEdit);
+        $scope.weightOptionsEdit = {delete: true};
+    };
+
+    $scope.$watch('weightOptions', function (newValue, oldValue, scope) {
+        if (oldValue == newValue) return;
+        if (!$scope.weightEdit) $scope.weightEdit = true;
+    }, true);
+
+    //  查询数据
+    $scope.weightQueryList = function () {
+
+        httpService.getAll('studySetting/getTemplateData').then(function (data) {
+
+            $scope.weightOptions = data || [];
+            $timeout(function () {
+                $scope.weightEdit = false;
+            });
+
+        }, function () {
+            SweetAlert.error('数据加载失败', '请检查网络');
+        })
+
+    };
+    $scope.weightQueryList();
+
+    $scope.saveWeightData = function () {
+        var data = [];
+        $.extend(true, data, $scope.weightOptions);
+        for (var i in data){
+            delete data[i].$$hashKey;
+            for (var j in data[i].value){
+                delete data[i].value[j].$$hashKey;
+            }
+        }
+        httpService.addRow('studySetting/saveTemplateData', {params: JSON.stringify(data)}).then(function (data) {
+            if (data > 0) {
+                $scope.weightQueryList();
+            }else {
+                SweetAlert.error('保存失败', '请检查网络');
+            }
+        }, function () {
+            SweetAlert.error('保存失败', '请检查网络');
+        });
+    };
+
 }]);
